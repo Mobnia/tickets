3edb15398975babced4966ef7877e2c51b21a8e6
ifenna <jawagu@gmail.com>
2018-11-09 16:05:42 +0100

    Add ability to get all teams

---
+++ app/Controllers/Teams/TeamController.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+namespace App\Controllers\Teams;
+
+
+use App\Controllers\BaseController;
+use App\Models\Team;
+use Zend\Diactoros\Response;
+use Zend\Diactoros\ServerRequest as Request;
+
+/**
+ * Class TeamController
+ *
+ * @package \App\Controllers\Teams
+ */
+class TeamController extends BaseController
+{
+    private $team;
+
+    public function __construct(Request $request, Response $response, Team $team)
+    {
+        $this->team = $team;
+
+        parent::__construct($request, $response);
+    }
+
+    public function getTeams()
+    {
+        $teams = $this->getEverything($this->team);
+        return $this->convertObjectToArray($teams);
+    }
+}
---
+++ tests/unit/Controllers/TeamControllerTest.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: ifenna
+ * Date: 11/9/18
+ * Time: 3:38 PM
+ */
+
+namespace App\Test\unit\Controllers;
+
+
+use App\Controllers\Teams\TeamController;
+use App\Models\Team;
+
+class TeamControllerTest extends BaseController
+{
+    private $team;
+    private $teamController;
+
+    public function __construct(?string $name = null, array $data = [], string $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName); // request and response are constructed in the parent
+
+        $this->team = new Team();
+        $this->teamController = new TeamController($this->request, $this->response, $this->team);
+    }
+
+    public function testGetTeams()
+    {
+        $this->assertArrayHasKey('data', $this->teamController->getTeams());
+    }
+}
