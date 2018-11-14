a480cdcf29dfb7367814249da9a86607bf327f24
ifenna <jawagu@gmail.com>
2018-11-14 13:31:14 +0100

    Remove unnecessary model logic

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -13,14 +13,4 @@ class Ticket extends Base
     protected $table = 'sporting_event_ticket';
 
     const MORPH_NAME = 'ticket';
-
-    public function event()
-    {
-        return $this->belongsTo(Event::class, 'sporting_event_id');
-    }
-
-    public function location()
-    {
-        return $this->belongsTo(Location::class, 'sport_location_id');
-    }
 }
