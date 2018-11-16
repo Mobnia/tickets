7d085fa9b5b99ab83fc33bd07e5662e6017a2d10
ifenna <jawagu@gmail.com>
2018-11-16 15:42:07 +0100

    Add authentication middleware skeleton

---
+++ app/Middleware/AuthenticationMiddleware.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace App\Middleware;
+
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+/**
+ * Class AuthenticationMiddleware
+ *
+ * @package \App\Middleware
+ */
+class AuthenticationMiddleware implements MiddlewareInterface
+{
+
+    /**
+     * Process an incoming server request and return a response, optionally delegating
+     * response creation to a handler.
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        // TODO: Implement process() method.
+        return $handler->handle($request);
+    }
+}
--- config/routes.php
+++ config/routes.php
@@ -29,11 +29,15 @@ $router->group('/', function (RouteGroup $router) {
     $router->get('/events', '\App\Controllers\Events\EventController::getUpcomingEvents');
     $router->get('/events/{id:number}', '\App\Controllers\Events\EventController::getEvent');
     $router->get('/events/{id:number}/tickets', '\App\Controllers\Tickets\TicketController::getTicketsForEvent');
+
     $router->put('/tickets/{id:number}', '\App\Controllers\Tickets\TicketPurchaseController::buyTicket');
+
     $router->get('/teams', '\App\Controllers\Teams\TeamController::getTeams');
     $router->get('/teams/{id:number}', '\App\Controllers\Teams\TeamController::getTeam');
+
     $router->get('/locations', '\App\Controllers\Locations\LocationController::getLocations');
     $router->get('/locations/{id:number}', '\App\Controllers\Locations\LocationController::getLocation');
-});
+
+})->middleware(new \App\Middleware\AuthenticationMiddleware());
 
 return $router;
